4f041a5f644e7bdd884c3d229f076e3d4f9c660f
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-05-13 04:36:12 +0500

    Created example2 demonstrating constructors

---
+++ basic-objects/example2.js
@@ -0,0 +1,13 @@
+const person1 = {
+  name: "Chris",
+  introduceSelf() {
+    console.log(`Hi! I'm ${this.name}.`);
+  },
+};
+
+const person2 = {
+  name: "Deepti",
+  introduceSelf() {
+    console.log(`Hi! I'm ${this.name}.`);
+  },
+};
