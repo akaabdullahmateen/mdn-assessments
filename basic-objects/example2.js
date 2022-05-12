b171233d30bb51d3ab2d1189a50e97ae4841c31f
akaAbdullahMateen <akaabdullahmateen@gmail.com>
2022-05-13 04:48:46 +0500

    Comparison of object literals vs functions vs constructors

--- basic-objects/example2.js
+++ basic-objects/example2.js
@@ -1,3 +1,5 @@
+// Manually typing object literals
+
 const person1 = {
   name: "Chris",
   introduceSelf() {
@@ -11,3 +13,42 @@ const person2 = {
     console.log(`Hi! I'm ${this.name}.`);
   },
 };
+
+person1.introduceSelf();
+person2.introduceSelf();
+
+// Functions to create objects
+
+function createPerson(name) {
+  const obj = {};
+  obj.name = name;
+  obj.introduceSelf = function () {
+    console.log(`Hi! I'm ${this.name}.`);
+  };
+  return obj;
+}
+
+const salva = createPerson("Salva");
+console.log(salva.name);
+salva.introduceSelf();
+
+const frankie = createPerson("Frankie");
+console.log(frankie.name);
+frankie.introduceSelf();
+
+// Constructor to create objects
+
+function Person(name) {
+  this.name = name;
+  this.introduceSelf = function () {
+    console.log(`Hi! I'm ${this.name}.`);
+  };
+}
+
+const salvaM = new Person("Salva");
+console.log(salvaM.name);
+salvaM.introduceSelf();
+
+const frankieM = new Person("Frankie");
+console.log(frankieM.name);
+frankieM.introduceSelf();
